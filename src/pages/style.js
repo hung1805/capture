7dcd50aa0441278b7f9ecac2d8eb348003f6386d
Hung Dinh Trong <dinhhung18052000@gmail.com>
2021-11-13 20:14:25 +0700

    first commit

---
+++ src/pages/style.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: stretch;
+  padding: 5rem 10rem;
+  color: #ffffff;
+`;
+export const Description = styled.div`
+  flex: 1;
+  /* display: flex;
+  flex-direction: column;  */
+`;
+export const AboutImage = styled.div`
+  flex: 1;
+  margin-left: 2rem;
+  overflow: hidden;
+`;
+export const Image = styled.img`
+  width: 100%;
+  height: 85vh;
+  object-fit: cover;
+`;
+export const Hide = styled.div`
+  overflow: hidden;
+`;
